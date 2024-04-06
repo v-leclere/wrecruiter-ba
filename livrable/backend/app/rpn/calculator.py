fa001f2e759531624db51f5ccea9575b24e17917
Valentin Leclère <vleclere.pro@gmail.com>
2024-04-06 18:58:30 +0200

    Split rpn to package

--- livrable/backend/app/rpn/calculator.py
+++ livrable/backend/app/rpn/calculator.py
@@ -1,6 +1,7 @@
 import re
 import string
-from typing import Self
+
+from app.rpn.pile import Stack
 
 
 def subtract(left_operand, right_operand):
@@ -25,50 +26,6 @@ def floor_divide(left_operand, right_operand):
 
 def divide(left_operand, right_operand):
     return left_operand / right_operand
-
-
-class Element:
-    def __init__(self, value):
-        self.__next = None
-        self.__data = value
-
-    def set_next(self, element):
-        self.__next = element
-
-    def get_data(self):
-        return self.__data
-
-    def get_next(self) -> Self | None:
-        return self.__next
-
-
-class Stack:
-    def __init__(self):
-        self.__head = None
-        self.__size = 0
-
-    def push(self, elem: string) -> None:
-        new_node = Element(elem)
-        new_node.set_next(self.__head)
-        self.__head = new_node
-        self.__size += 1
-
-    def pop(self) -> string:
-        to_pop = self.__head
-        self.__head = to_pop.get_next()
-        return_value = to_pop.get_data()
-        del to_pop
-        self.__size -= 1
-        return return_value
-
-    def top(self) -> Element:
-        return self.__head
-
-    def is_empty(self) -> bool:
-        return self.__size == 0
-
-    def size(self) -> int:
-        return self.__size
 
 
 class Calculator:
